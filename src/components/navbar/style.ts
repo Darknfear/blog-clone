2934bdb58cd7fbd4988ee16e00848a1b7544ceda
Hiếu Nguyễn <hieuact13011998@gmail.com>
2022-06-09 14:44:37 +0700

    create navbar

---
+++ src/components/navbar/style.ts
@@ -0,0 +1,21 @@
+import styled from 'styled-components';
+
+const Container = styled.div`
+  height: 70px;
+  background: #090b13;
+  display: flex;
+  align-items: center;
+  padding: 15px 50px;
+`;
+
+const Menu = styled.div`
+    display: flex;
+    flex: 1;
+    align-items: center;
+`
+
+const Logo = styled.img`
+    color: #000;
+`
+
+export { Container, Menu, Logo };
